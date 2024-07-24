0e20046d976acee070fbcfd82d29e62c672b9748
apion <apion@apionservers.com>
2024-07-23 21:13:43 -0400

    Move forge MDK to neoforge MDK

--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("dev.architectury.loom") version("1.2-SNAPSHOT")
+    id("dev.architectury.loom") version("1.6-SNAPSHOT")
     id("architectury-plugin") version("3.4-SNAPSHOT")
 }
 
@@ -10,15 +10,11 @@ version = "1.0-SNAPSHOT"
 
 architectury {
     platformSetupLoomIde()
-    forge()
+    neoForge()
 }
 
 loom {
     silentMojangMappingsLicense()
-
-    mixin {
-        defaultRefmapName.set("mixins.${project.name}.refmap.json")
-    }
 }
 
 repositories {
@@ -27,15 +23,19 @@ repositories {
     maven("https://maven.impactdev.net/repository/development/")
     maven("https://hub.spigotmc.org/nexus/content/groups/public/")
     maven("https://thedarkcolour.github.io/KotlinForForge/")
+    maven("https://maven.neoforged.net")
 }
 
 dependencies {
-    minecraft("net.minecraft:minecraft:1.20.1")
+    minecraft("net.minecraft:minecraft:1.21")
     mappings(loom.officialMojangMappings())
-    forge("net.minecraftforge:forge:1.20.1-47.2.0")
+    neoForge("net.neoforged:neoforge:21.0.117-beta")
 
-    modImplementation("com.cobblemon:forge:1.4.0+1.20.1-SNAPSHOT")
-    runtimeOnly("thedarkcolour:kotlinforforge:4.5.0")
+    modImplementation("com.cobblemon:neoforge:1.6.0+1.21-SNAPSHOT")
+    //Needed for cobblemon
+    implementation("thedarkcolour:kotlinforforge-neoforge:5.3.0") {
+        exclude(group = "net.neoforged.fancymodloader", module = "loader")
+    }
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
--- neoforge/src/main/java/com/cobblemon/example/ExampleNeoForgeMod.java
+++ neoforge/src/main/java/com/cobblemon/example/ExampleNeoForgeMod.java
@@ -6,23 +6,24 @@ import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.event.RegisterCommandsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
 
 @Mod("cobblemon_forge_mdk")
-public final class ExampleForgeMod {
+public final class ExampleNeoForgeMod {
 
-    public ExampleForgeMod() {}
+    public ExampleNeoForgeMod() {}
 
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
+    @EventBusSubscriber
     public static final class Registration {
 
         @SubscribeEvent
         public static void onCommandRegistration(final RegisterCommandsEvent event) {
             event.getDispatcher().register(Commands.literal("test")
                     .executes(context -> {
-                        Species species = PokemonSpecies.INSTANCE.getByIdentifier(ResourceLocation.of("cobblemon:bidoof", ':'));
+                        Species species = PokemonSpecies.INSTANCE.getByIdentifier(ResourceLocation.tryParse("cobblemon:bidoof"));
                         context.getSource().sendSystemMessage(
                                 Component.literal("Got species: ")
                                         .withStyle(Style.EMPTY.withColor(0x03e3fc))
